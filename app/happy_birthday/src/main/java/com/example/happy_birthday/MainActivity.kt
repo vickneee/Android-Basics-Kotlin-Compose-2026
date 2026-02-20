1ed3158ebd6edbe481dfa4fae0a79738b49f760d
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2026-02-20 20:26:44 +0200

    Update alignment

--- app/happy_birthday/src/main/java/com/example/happy_birthday/MainActivity.kt
+++ app/happy_birthday/src/main/java/com/example/happy_birthday/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -63,7 +64,7 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
             text = from,
             fontSize = 36.sp,
             modifier = Modifier
-                .padding(16.dp)
+                .padding(16.dp).align(Alignment.End)
         )
     }
 }
