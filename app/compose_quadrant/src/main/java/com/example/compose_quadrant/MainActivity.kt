acc78b2a6d80e8185380fa711142282889ae0181
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2026-02-23 00:05:28 +0200

    Initial commit

--- app/compose_quadrant/src/main/java/com/example/compose_quadrant/MainActivity.kt
+++ app/compose_quadrant/src/main/java/com/example/compose_quadrant/MainActivity.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.compose_quadrant.ui.theme.AndroidBasicsKotlinCompose2026Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +31,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AndroidBasicsKotlinCompose2026Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Quadrant(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ComposeQuadrantApp()
                 }
             }
         }
@@ -31,17 +43,71 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Quadrant(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ComposeQuadrantApp() {
+    Column(Modifier.fillMaxWidth()) {
+        Row(Modifier.weight(1f)) {
+            ComposableInfoCard(
+                title = stringResource(R.string.text_composable),
+                description = stringResource(R.string.first_description),
+                backgroundColor = Color(0xFFEADDFF),
+                modifier = Modifier.weight(1f)
+            )
+            ComposableInfoCard(
+                title = stringResource(R.string.image_composable),
+                description = stringResource(R.string.second_description),
+                backgroundColor = Color(0xFFD0BCFF),
+                modifier = Modifier.weight(1f)
+            ) }
+        Row(Modifier.weight(1f)) {
+            ComposableInfoCard(
+                title = stringResource(R.string.row_composable),
+                description = stringResource(R.string.third_description),
+                backgroundColor = Color(0xFFB69DF8),
+                modifier = Modifier.weight(1f)
+            )
+            ComposableInfoCard(
+                title = stringResource(R.string.column_composable),
+                description = stringResource(R.string.fourth_description),
+                backgroundColor = Color(0xFFF6EDFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
 }
+
+ @Composable
+ private fun ComposableInfoCard(
+     title: String,
+     description: String,
+     backgroundColor: Color,
+     modifier: Modifier = Modifier
+ ) {
+     Column(
+         modifier = modifier
+             .fillMaxSize()
+             .background(backgroundColor)
+             .padding(16.dp),
+         verticalArrangement = Arrangement.Center,
+         horizontalAlignment = Alignment.CenterHorizontally
+     ) {
+         Text(
+             text = title,
+             modifier = Modifier.padding(bottom = 16.dp),
+             fontWeight = FontWeight.Bold,
+             color = Color.Black
+         )
+         Text(
+             text = description,
+             textAlign = TextAlign.Justify,
+             color = Color.Black
+         )
+     }
+ }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AndroidBasicsKotlinCompose2026Theme {
-        Quadrant("Android")
+        ComposeQuadrantApp()
     }
 }
