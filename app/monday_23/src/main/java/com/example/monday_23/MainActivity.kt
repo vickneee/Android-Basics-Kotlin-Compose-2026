ffd198ec430e5f4bf6210f6d4dc592fc67785e6b
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2026-02-23 11:11:36 +0200

    Initial commit

---
+++ app/monday_23/src/main/java/com/example/monday_23/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.example.monday_23
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.monday_23.ui.theme.AndroidBasicsKotlinCompose2026Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AndroidBasicsKotlinCompose2026Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    ModifyText(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ModifyText(modifier: Modifier = Modifier) {
+    // var text by remember { mutableStateOf("") }
+    var text = "Huh Hei"
+    Column(
+        modifier = modifier
+            .padding(36.dp)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Greeting(
+            name = text,
+            modifier = Modifier
+        )
+        TextField(
+            value = text,
+            onValueChange = { text = it },
+            label = { Text(stringResource(R.string.enter_name)) },
+        )
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AndroidBasicsKotlinCompose2026Theme {
+        ModifyText()
+    }
+}
