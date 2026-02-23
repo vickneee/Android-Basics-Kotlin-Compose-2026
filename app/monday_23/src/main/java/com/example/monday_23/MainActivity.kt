f4457d4ca533bab5f4e1774cf819dc4517081f39
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2026-02-23 11:27:29 +0200

    Initial commit

--- app/monday_23/src/main/java/com/example/monday_23/MainActivity.kt
+++ app/monday_23/src/main/java/com/example/monday_23/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.monday_23
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -12,6 +13,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -35,8 +40,8 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ModifyText(modifier: Modifier = Modifier) {
-    // var text by remember { mutableStateOf("") }
-    var text = "Huh Hei"
+    var text by remember { mutableStateOf("") }
+    // var text = "Huh Hei"
     Column(
         modifier = modifier
             .padding(36.dp)
@@ -50,7 +55,7 @@ fun ModifyText(modifier: Modifier = Modifier) {
         )
         TextField(
             value = text,
-            onValueChange = { text = it },
+            onValueChange = { text = it; Log.i("JV", "Arvo on $it") },
             label = { Text(stringResource(R.string.enter_name)) },
         )
     }
