914e89adf296805e15320587fc64aacf889633f4
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2026-02-26 00:55:36 +0200

    Initial comment

--- app/dice_roller/src/main/java/com/example/dice_roller/MainActivity.kt
+++ app/dice_roller/src/main/java/com/example/dice_roller/MainActivity.kt
@@ -4,15 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.dice_roller.ui.theme.AndroidBasicsKotlinCompose2026Theme
 
@@ -48,6 +53,13 @@ fun DiceRollerApp(modifier: Modifier = Modifier) {
 
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
-    Text("Dice")
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(painter = painterResource(R.drawable.dice_1), contentDescription = "1")
+        Button(onClick = { /*TODO*/ }) {
+            Text(stringResource(R.string.roll))
+        }
+    }
 }
 
