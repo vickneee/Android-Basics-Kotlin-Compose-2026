fcd5763beb44b8ecdf820b38d70ae238d9fd6afe
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2026-02-26 01:26:26 +0200

    Update code, add painterResource

--- app/dice_roller/src/main/java/com/example/dice_roller/MainActivity.kt
+++ app/dice_roller/src/main/java/com/example/dice_roller/MainActivity.kt
@@ -12,16 +12,22 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.dice_roller.ui.theme.AndroidBasicsKotlinCompose2026Theme
 
 class MainActivity : ComponentActivity() {
@@ -34,7 +40,8 @@ class MainActivity : ComponentActivity() {
                     Surface(
                         modifier = Modifier
                             .fillMaxSize()
-                            .padding(innerPadding)
+                            .padding(innerPadding),
+                        color = MaterialTheme.colorScheme.background
                     ) {
                         DiceRollerApp()
                     }
@@ -46,7 +53,7 @@ class MainActivity : ComponentActivity() {
 
 @Preview(showBackground = true)
 @Composable
-fun DiceRollerApp(modifier: Modifier = Modifier) {
+fun DiceRollerApp() {
     DiceWithButtonAndImage(
         modifier = Modifier
             .fillMaxSize()
@@ -56,15 +63,25 @@ fun DiceRollerApp(modifier: Modifier = Modifier) {
 
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+    var result by remember { mutableIntStateOf(1) }
+    val imageResource = when (result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally) {
         Image(painter = painterResource(
-            R.drawable.dice_1),
-            contentDescription = "1")
+            imageResource),
+            contentDescription = result.toString())
         Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { /*TODO*/ }) {
-            Text(stringResource(R.string.roll))
+        Button(onClick = { result = (1..6).random() },
+            ) {
+            Text(text = stringResource(R.string.roll), fontSize = 24.sp)
         }
     }
 }
