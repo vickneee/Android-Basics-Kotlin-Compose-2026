7645fee2652585713d5a1db696d5cb2243e2a885
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2026-03-03 13:53:09 +0200

    Initial commit

--- app/event_app/src/main/java/com/example/event_app/MainActivity.kt
+++ app/event_app/src/main/java/com/example/event_app/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AndroidBasicsKotlinCompose2026Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+                    EventApp(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
@@ -31,7 +31,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun EventApp(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
         modifier = modifier
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     AndroidBasicsKotlinCompose2026Theme {
-        Greeting("Android")
+        EventApp("Android")
     }
 }
