7612708638e26023097501256bccc91fc640c661
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2026-03-03 14:51:41 +0200

    Add app functionality

--- app/affirmations/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/affirmations/src/main/java/com/example/affirmations/MainActivity.kt
@@ -6,21 +6,32 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.affirmations.data.Datasource
 import com.example.affirmations.model.Affirmation
 import com.example.affirmations.ui.theme.AndroidBasicsKotlinCompose2026Theme
 
@@ -42,7 +53,31 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun AffirmationsApp(modifier: Modifier = Modifier) {
-    Text(text = "Affirmations")
+    val layoutDirection = LocalLayoutDirection.current
+    Surface(modifier = Modifier.fillMaxSize()
+        .statusBarsPadding()
+        .padding(
+            start = WindowInsets.safeDrawing.asPaddingValues()
+                .calculateStartPadding(layoutDirection),
+            end = WindowInsets.safeDrawing.asPaddingValues()
+                .calculateEndPadding(layoutDirection),
+        )) {
+        AffirmationList(
+            affirmationList = Datasource().loadAffirmations(),
+        )
+    }
+}
+
+@Composable
+fun AffirmationList(affirmationList: List<Affirmation>, modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(affirmationList) { affirmation ->
+            AffirmationCard(
+                affirmation = affirmation,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
 }
 
 @Composable
