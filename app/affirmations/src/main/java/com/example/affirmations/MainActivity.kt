83c893c2f5598fb6eb1201afab6496c622e7f7bf
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2026-03-03 14:36:13 +0200

    Add Images

--- app/affirmations/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/affirmations/src/main/java/com/example/affirmations/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.affirmations.model.Affirmation
 import com.example.affirmations.ui.theme.AndroidBasicsKotlinCompose2026Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +31,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AndroidBasicsKotlinCompose2026Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    AffirmationsApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +41,33 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun AffirmationsApp(modifier: Modifier = Modifier) {
+    Text(text = "Affirmations")
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    AndroidBasicsKotlinCompose2026Theme {
-        Greeting("Android")
+fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {
+    Card(modifier = modifier) {
+        Column() {
+            Image(
+                painter = painterResource(affirmation.imageResourceId),
+                contentDescription = stringResource(affirmation.stringResourceId),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(194.dp),
+                contentScale = ContentScale.Crop
+            )
+            Text(
+                text = stringResource(affirmation.stringResourceId),
+                modifier = Modifier.padding(16.dp),
+                style = MaterialTheme.typography.headlineSmall
+            )
+        }
     }
+}
+
+@Preview()
+@Composable
+fun AffirmationCardPreview() {
+    AffirmationCard(Affirmation(R.string.affirmation1, R.drawable.image1))
 }
